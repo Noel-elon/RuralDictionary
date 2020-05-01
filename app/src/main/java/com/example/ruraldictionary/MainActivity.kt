cc47adb3601bd7ef460ac76173db895c80b3c840
Noel-elon <noelnwaelugo@gmail.com>
2020-05-01 21:48:50 +0100

    bottom Nav 2

--- app/src/main/java/com/example/ruraldictionary/MainActivity.kt
+++ app/src/main/java/com/example/ruraldictionary/MainActivity.kt
@@ -2,13 +2,14 @@ package com.example.ruraldictionary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.ruraldictionary.Repository.WordObjectViewModel
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +17,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        val bottomnav = bottomNav.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    return@setOnNavigationItemSelectedListener true
+                }
 
+                R.id.navigation_fav -> {
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+            }
+            false
+
+        }
+      //  bottomNav.setOnNavigationItemSelectedListener(bottomnav)
 
 
     }
